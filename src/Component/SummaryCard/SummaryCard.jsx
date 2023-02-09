a23f75afe51b071a645a0852f9739f53cbe2dd85
sh-mihad <sh.mihad.cse@gmail.com>
2023-02-10 01:25:40 +0600

    summary-card designe done in dynamicly

--- src/Component/SummaryCard/SummaryCard.jsx
+++ src/Component/SummaryCard/SummaryCard.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import { BsFillPieChartFill } from "react-icons/bs";
+import { BsFillPieChartFill,BsHandbagFill } from "react-icons/bs";
+import { FaUserFriends } from "react-icons/fa";
 
 
-const SummaryCard = ({icon,title,actualData,percantige})=>{
+const SummaryCard = ({title,actualData,percantige})=>{
+
+    console.log(parseFloat(percantige))
     return (
         <div className='flex bg-white p-5  rounded-lg gap-5 drop-shadow-md'>
-            <div className='p-3 rounded-lg bg-[#2c698d] text-xl text-white'><BsFillPieChartFill/></div>
+            <div className='p-3 rounded-lg bg-[#2c698d] text-xl text-white self-center'>
+               
+               {
+                title === "Revenue" && < BsFillPieChartFill/> 
+                
+               }
+               {
+                title === "Orders" && < BsHandbagFill/> 
+
+               }
+               {
+                title === "Customers" && < FaUserFriends/> 
+
+               }
+               </div>
 
             <div>
-            <p className='text-sm font-light text-gray-500 '>Revenue</p>
+            <p className='text-sm font-light text-gray-500 '>{title}</p>
             <div className='flex gap-7 items-center'>
-            <h6 className='text-xl font-semibold'>$21,456</h6>
-            <p className='text-xs font-light text-gray-500 bg-green-200 px-3 rounded-lg'>+ 2.65%</p>
+            <h6 className='text-xl font-semibold'>{actualData}</h6>
+            <p className={`text-xs font-light text-gray-500 bg-green-200 px-3 rounded-lg ${parseFloat(percantige)<1 ? "bg-red-400 text-black" : "bg-green-300"}`}>{percantige}</p>
             </div>
                
             </div>
--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import SummaryCard from '../../Component/SummaryCard/SummaryCard';
+import { BsFillPieChartFill } from "react-icons/bs";
 
 const Dashboard = () => {
     return (
-        <div className='block lg:flex '>
-            <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
-                <SummaryCard/>
-                <SummaryCard/>
-                <SummaryCard/>
+        <div className='block lg:flex gap-5'>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-6 '>
+                <SummaryCard  title={"Revenue"} actualData={"$21,456"} percantige={"+2.65%"}/>
+                <SummaryCard title={"Orders"} actualData={"5,643"} percantige={"-0.82%"}/>
+                <SummaryCard title={"Customers"} actualData={"45,254"} percantige={"-1.04%"}/>
             </div>
             <div>profile content</div>
         </div>
